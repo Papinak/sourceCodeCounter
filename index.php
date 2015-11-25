ff5469d5fef4ffa1f92e93648d11386e6ca8ce8a
Papinak <papicz97@gmail.com>
2015-11-25 21:13:53 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<style>
+		li{list-style-type: none;}
+		</style>
+		<?php if(empty($_GET)): ?>
+
+			<form action="index.php" method="GET">
+ 				<?php
+ 					$id = 0;
+ 					$class = 0;
+ 					$dirSystem = array();
+ 					$lastDirSystem = array();
+ 					$lastIsDir = DIRECTORY_SEPARATOR;
+ 					$showPath = "";
+					foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator('.')) as $path)
+					{
+						echo("$path<br>\n");
+					}
+					echo("\t\t\t\t</ol></ol>\n")					
+ 				?>
+ 				<input type="submit" value="OK">
+			</form>
+		<?php  
+			else:
+				
+			endif;
+		?>
+	</body>
+</html>
